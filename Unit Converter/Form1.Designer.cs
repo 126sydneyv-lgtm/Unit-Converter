b61bedd3dc262aa7c3999c231287664010349365
Sydney Valliere <126.sydney.v@gmail.com>
2025-10-30 11:07:56 -0400

    finished

--- Unit Converter/Form1.Designer.cs
+++ Unit Converter/Form1.Designer.cs
@@ -28,6 +28,7 @@
         /// </summary>
         private void InitializeComponent()
         {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
             this.label1 = new System.Windows.Forms.Label();
             this.label2 = new System.Windows.Forms.Label();
             this.label3 = new System.Windows.Forms.Label();
@@ -38,82 +39,89 @@
             this.choiceTextbox = new System.Windows.Forms.TextBox();
             this.valueTextbox = new System.Windows.Forms.TextBox();
             this.convertButton = new System.Windows.Forms.Button();
-            this.label8 = new System.Windows.Forms.Label();
+            this.resultLabel = new System.Windows.Forms.Label();
             this.SuspendLayout();
             // 
             // label1
             // 
             this.label1.AutoSize = true;
-            this.label1.Location = new System.Drawing.Point(168, 25);
+            this.label1.Font = new System.Drawing.Font("Arial Rounded MT Bold", 16.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(110, 18);
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(108, 16);
+            this.label1.Size = new System.Drawing.Size(248, 32);
             this.label1.TabIndex = 0;
             this.label1.Text = "Unit Conversions";
             // 
             // label2
             // 
             this.label2.AutoSize = true;
-            this.label2.Location = new System.Drawing.Point(152, 64);
+            this.label2.Font = new System.Drawing.Font("Century", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(130, 64);
             this.label2.Name = "label2";
-            this.label2.Size = new System.Drawing.Size(147, 16);
+            this.label2.Size = new System.Drawing.Size(206, 21);
             this.label2.TabIndex = 1;
             this.label2.Text = "1. Inches to Centimetres";
             // 
             // label3
             // 
             this.label3.AutoSize = true;
-            this.label3.Location = new System.Drawing.Point(152, 89);
+            this.label3.Font = new System.Drawing.Font("Century", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.Location = new System.Drawing.Point(130, 89);
             this.label3.Name = "label3";
-            this.label3.Size = new System.Drawing.Size(135, 16);
+            this.label3.Size = new System.Drawing.Size(190, 21);
             this.label3.TabIndex = 2;
             this.label3.Text = "2. Feet to Centimetres";
             // 
             // label4
             // 
             this.label4.AutoSize = true;
-            this.label4.Location = new System.Drawing.Point(152, 114);
+            this.label4.Font = new System.Drawing.Font("Century", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label4.Location = new System.Drawing.Point(130, 114);
             this.label4.Name = "label4";
-            this.label4.Size = new System.Drawing.Size(114, 16);
+            this.label4.Size = new System.Drawing.Size(159, 21);
             this.label4.TabIndex = 3;
             this.label4.Text = "3. Yards to Metres";
             // 
             // label5
             // 
             this.label5.AutoSize = true;
-            this.label5.Location = new System.Drawing.Point(152, 139);
+            this.label5.Font = new System.Drawing.Font("Century", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label5.Location = new System.Drawing.Point(130, 139);
             this.label5.Name = "label5";
-            this.label5.Size = new System.Drawing.Size(132, 16);
+            this.label5.Size = new System.Drawing.Size(186, 21);
             this.label5.TabIndex = 4;
             this.label5.Text = "4. Miles to Kilometres";
             // 
             // label6
             // 
             this.label6.AutoSize = true;
-            this.label6.Location = new System.Drawing.Point(94, 193);
+            this.label6.Font = new System.Drawing.Font("Century", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label6.Location = new System.Drawing.Point(72, 193);
             this.label6.Name = "label6";
-            this.label6.Size = new System.Drawing.Size(182, 16);
+            this.label6.Size = new System.Drawing.Size(246, 21);
             this.label6.TabIndex = 5;
             this.label6.Text = "Enter conversion choice (1-4):";
             // 
             // label7
             // 
             this.label7.AutoSize = true;
-            this.label7.Location = new System.Drawing.Point(94, 227);
+            this.label7.Font = new System.Drawing.Font("Century", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label7.Location = new System.Drawing.Point(72, 227);
             this.label7.Name = "label7";
-            this.label7.Size = new System.Drawing.Size(173, 16);
+            this.label7.Size = new System.Drawing.Size(236, 21);
             this.label7.TabIndex = 6;
             this.label7.Text = "Enter value to be converted:";
             // 
             // choiceTextbox
             // 
-            this.choiceTextbox.Location = new System.Drawing.Point(282, 193);
+            this.choiceTextbox.Location = new System.Drawing.Point(324, 194);
             this.choiceTextbox.Name = "choiceTextbox";
             this.choiceTextbox.Size = new System.Drawing.Size(100, 22);
             this.choiceTextbox.TabIndex = 7;
             // 
             // valueTextbox
             // 
-            this.valueTextbox.Location = new System.Drawing.Point(282, 224);
+            this.valueTextbox.Location = new System.Drawing.Point(324, 227);
             this.valueTextbox.Name = "valueTextbox";
             this.valueTextbox.Size = new System.Drawing.Size(100, 22);
             this.valueTextbox.TabIndex = 8;
@@ -128,21 +136,20 @@
             this.convertButton.UseVisualStyleBackColor = true;
             this.convertButton.Click += new System.EventHandler(this.convertButton_Click);
             // 
-            // label8
+            // resultLabel
             // 
-            this.label8.AutoSize = true;
-            this.label8.Location = new System.Drawing.Point(201, 315);
-            this.label8.Name = "label8";
-            this.label8.Size = new System.Drawing.Size(44, 16);
-            this.label8.TabIndex = 10;
-            this.label8.Text = "label8";
+            this.resultLabel.Location = new System.Drawing.Point(97, 315);
+            this.resultLabel.Name = "resultLabel";
+            this.resultLabel.Size = new System.Drawing.Size(281, 16);
+            this.resultLabel.TabIndex = 10;
+            this.resultLabel.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(475, 450);
-            this.Controls.Add(this.label8);
+            this.ClientSize = new System.Drawing.Size(475, 369);
+            this.Controls.Add(this.resultLabel);
             this.Controls.Add(this.convertButton);
             this.Controls.Add(this.valueTextbox);
             this.Controls.Add(this.choiceTextbox);
@@ -153,8 +160,9 @@
             this.Controls.Add(this.label3);
             this.Controls.Add(this.label2);
             this.Controls.Add(this.label1);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
             this.Name = "Form1";
-            this.Text = "Form1";
+            this.Text = "Unit Conversions";
             this.Load += new System.EventHandler(this.Form1_Load);
             this.ResumeLayout(false);
             this.PerformLayout();
@@ -173,7 +181,7 @@
         private System.Windows.Forms.TextBox choiceTextbox;
         private System.Windows.Forms.TextBox valueTextbox;
         private System.Windows.Forms.Button convertButton;
-        private System.Windows.Forms.Label label8;
+        private System.Windows.Forms.Label resultLabel;
     }
 }
 
--- Unit Converter/Form1.cs
+++ Unit Converter/Form1.cs
@@ -12,37 +12,36 @@ namespace Unit_Converter
 {
     public partial class Form1 : Form
     {
-        int conversionChoice;
-        int value;
-       
+        int choice;
+        double value;
+
         public Form1()
         {
             InitializeComponent();
         }
 
-        private double InchesToCm(double conversionValue)
+        double InchesToCm(double inches)
         {
-            if (conversionChoice == 1)
-            {
-                value = Convert.ToInt32(valueTextbox);
-                return conversionValue = value * 2.54;
-               
-            }
+            double cm = inches * 2.54;
+            return cm;
         }
 
-        private void FeetToCm()
+        double FeetToCm(double feet)
         {
-
+            double cm = feet * 30.48;
+            return cm;
         }
 
-        private void YardsToMetres()
+        double YardsToMetres(double yards)
         {
-
+            double metres = yards * 0.91;
+            return metres;
         }
 
-        private void MilesToKm()
+        double MilesToKm(double miles)
         {
-
+            double km = miles * 1.6;
+            return km;
         }
         private void Form1_Load(object sender, EventArgs e)
         {
@@ -51,7 +50,38 @@ namespace Unit_Converter
 
         private void convertButton_Click(object sender, EventArgs e)
         {
-            conversionChoice = Convert.ToInt32(choiceTextbox);
+            choice = Convert.ToInt16(choiceTextbox.Text);
+            value = Convert.ToDouble(valueTextbox.Text);
+            double result = 0;
+            string unit;
+            string unit2;
+            if (choice == 1)
+            {
+                result = InchesToCm(value);
+                unit = "inches";
+                unit2 = "cm";
+            }
+            else if (choice == 2)
+            {
+                result = FeetToCm(value);
+                unit = "feet";
+                unit2 = "cm";
+            }
+            else if (choice == 3)
+            {
+                result = YardsToMetres(value);
+                unit = "yards";
+                unit2 = "metres";
+            }
+            else
+            {
+                result = MilesToKm(value);
+                unit = "miles";
+                unit2 = "km";
+            }
+
+            //display result
+            resultLabel.Text = "" + value + " " + unit + " = " + result.ToString("F2") + " " + unit2;
         }
     }
 }
